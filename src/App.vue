554b27c80963c4fa024c7955d644c3473491e186
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-02-25 21:49:50 +0530

    add pinia store

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import {useCounterStore} from './store/counter'
+
+const store = useCounterStore();
+
 </script>
 
 <template>
+<h1 @click="store.increment"> {{ store.count }}</h1>
  
 </template>
 
