97752e44eca9a5cb8a4c2a2edfadc39fcf9d104d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-02-27 05:07:02 +0530

    make components with typed props

--- src/components/home-page/CategoryList.vue
+++ src/components/home-page/CategoryList.vue
@@ -1,18 +1,21 @@
 <template>
     <h2 class="font-bold text-xl">{{ title }}</h2>
     <ul class="mt-4 tab">
-        <li><a class="block text-gray-700 py-2 tablinks">Category 1</a></li>
-        <li><a class="block text-gray-700 py-2 tablinks">Category 2</a></li>
-        <li><a class="block text-gray-700 py-2 tablinks">Category 3</a></li>
-        <li><a class="block text-gray-700 py-2 tablinks">Category 4</a></li>
+        <li v-for="(category,i) in categoryList" :key="i">
+            <a class="block text-gray-700 py-2 tablinks" @click.prevent="selectedCategory=category">{{ category }}</a>
+        </li>
     </ul>
 </template>
 
 <script setup lang="ts">
 
+import { ref,PropType } from 'vue';
+
+const selectedCategory = ref<string>('')
+
 const props = defineProps({
     title: {type: String , defualt:""},
-    categoryList: {type: Object , defualt:[]},
+    categoryList: {type: Object as PropType<Array<string>> , defualt:[]},
 })
 
 </script>
