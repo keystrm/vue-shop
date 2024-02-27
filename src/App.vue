0acd2582b7e19f1b97938eb2ea5b36ae2e98e08f
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-02-27 06:18:32 +0530

    clear unnesscery

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,5 @@
 <script setup lang="ts">
 import TopNavBar from './components/TopNavBar.vue'
 
-import Menu from 'primevue/menu';
-import { ref } from "vue";
-
-const items = ref([
-    { label: 'New', icon: 'pi pi-plus' },
-    { label: 'Search', icon: 'pi pi-search' }
-]);
 
 </script>
